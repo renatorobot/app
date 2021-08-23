cb6bcdfb4037bb68dd50fcd14e3a83a95f84443b
Renato Andrade <renatorobot@outlook.com.br>
2021-08-23 11:27:02 -0300

    Criando o primeiro componente em React Native

--- App.js
+++ App.js
@@ -5,11 +5,13 @@
  * @format
  * @flow strict-local
  */
-import React, { Component } from 'react';
-
+//import React, { Component } from 'react';
+import React from 'react';
 import {Text, View} from 'react-native';
+import C1 from './componentes/comp1'
 
 
+/*
 export default class App1 extends Component{
   render(){
     return(
@@ -26,22 +28,14 @@ export default class App1 extends Component{
     );
   }
 }
+*/
 
 
 
-/*
 export default function App1(){
   return (
     <View>
-      <View>
-        <Text>CFB CURSOS</Text>
-        <Text>Curso de React Native</Text>
-      </View>
-      <View>
-        <Text>Aula 3</Text>
-        <Text>www.cfbcursos.com.br</Text>
-      </View>
+      <C1/>
     </View>
   );
 };
-*/
