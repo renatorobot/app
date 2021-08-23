47275d38b444a3191d23b4c5db15dba1357f0cb5
Renato Andrade <renatorobot@outlook.com.br>
2021-08-23 10:52:58 -0300

    Primeiro Commit

---
+++ App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React, { Component } from 'react';
+
+import {Text, View} from 'react-native';
+
+
+export default class App1 extends Component{
+  render(){
+    return(
+      <View>
+      <View>
+        <Text>CFB CURSOS</Text>
+        <Text>Curso de React Native</Text>
+      </View>
+      <View>
+        <Text>Aula 3</Text>
+        <Text>www.cfbcursos.com.br</Text>
+      </View>
+    </View>
+    );
+  }
+}
+
+
+
+/*
+export default function App1(){
+  return (
+    <View>
+      <View>
+        <Text>CFB CURSOS</Text>
+        <Text>Curso de React Native</Text>
+      </View>
+      <View>
+        <Text>Aula 3</Text>
+        <Text>www.cfbcursos.com.br</Text>
+      </View>
+    </View>
+  );
+};
+*/
