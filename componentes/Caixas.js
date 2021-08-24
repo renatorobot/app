02239058126ada08ab772fb5ad787df5fd568797
Renato Andrade <renatorobot@outlook.com.br>
2021-08-24 10:45:29 -0300

    Elementos Responsivos, contêiners Flex em React Native

--- componentes/Caixas.js
+++ componentes/Caixas.js
@@ -6,10 +6,13 @@ export default function comp1 (props){
 
     return (
         
-        <View style={{width: '100%', height: 300, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <View style={{width: 50, height: 50, backgroundColor: '#00f'}}></View>
-            <View style={{width: 50, height: 50, backgroundColor: '#00a'}}></View>
-            <View style={{width: 50, height: 50, backgroundColor: '#005'}}></View>
+        <View style={{width: '100%', height: 300, flexDirection: 'column', alignItems: 'stretch', justifyContent: 'center'}}>
+            <View style={{flexGrow:1, backgroundColor: '#00f'}}></View>
+            <View style={{flexGrow:1, height: 50, backgroundColor: '#00a'}}></View>
+            <View style={{flexGrow:1, height: 50, backgroundColor: '#005'}}></View>
+            <View style={{flexGrow:1, height: 50, backgroundColor: '#f00'}}></View>
+            <View style={{flexGrow:1, height: 50, backgroundColor: '#a00'}}></View>
+            <View style={{flexGrow:1, height: 50, backgroundColor: '#500'}}></View>
         </View>
         
     )
