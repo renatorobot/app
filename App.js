177f8ccd429aed9c989f9d5593c7dc4809cea16d
Renato Andrade <renatorobot@outlook.com.br>
2021-09-02 09:59:28 -0300

    inserir imagem de fundo no aplicativo, ImageBackground

--- App.js
+++ App.js
@@ -7,19 +7,31 @@
  */
 //import React, { Component } from 'react';
 import React from 'react';
-import {StyleSheet, Text, View, Image, Button, Alert} from 'react-native';
+import {StyleSheet, Text, View, Image, Button, Alert, ImageBackground} from 'react-native';
 import C1 from './componentes/comp1';
 import Caixas from './componentes/Caixas';
 import Lp from './componentes/ListaPlana';
-import Estilos from './estilos/Estilos.js'
+import Estilos from './estilos/Estilos.js';
+
+const img1 = './assets/imagens/bg1.jpg';
+const img2 = './assets/imagens/bg2.jpg';
 
 
 export default function App1(){
   return (
     <View style={Estilos.conteiner}>
-      <Text>Curso de React Native</Text>
-      <Text>Curso de React Native</Text>
-      <Lp/>
+      <ImageBackground 
+
+        source={require(img2)}
+        style={estilos.imagenFund}
+
+      >
+        <View>
+          <Text>Curso de React Native</Text>
+          <Text>Curso de React Native</Text>
+          {/*<Lp/>*/ }
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -31,6 +43,13 @@ const estilos = StyleSheet.create({
     resizeMode: 'contain'
 
   
+  },
+
+  imagenFund:{
+    
+    flex: 1,
+    resizeMode: 'cover',
+    width: "100%",
   }
 
 })
