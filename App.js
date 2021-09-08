7e8ce2ac9e067a7095bc9f615b538bc586643088
Renato Andrade <renatorobot@outlook.com.br>
2021-09-08 10:43:34 -0300

    Componente Touchablehighlight

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View, Button, SafeAreaView} from 'react-native';
 import Estilos from './estilos/Estilos.js';
-import Rolagem from './componentes/Rolagem'
+import Toque from './componentes/Toque'
 
 
 export default function App1(){
@@ -20,7 +20,7 @@ export default function App1(){
 
       <Text>Curso de React Native</Text>
       <Text>Curso de React Native</Text>
-      <Rolagem/>
+      <Toque/>
   
     </SafeAreaView>
     
